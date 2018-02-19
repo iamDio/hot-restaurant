b3fb3705b8ed74c2a3fc2422d635508fb4c36f86
iamDio <claudio.villani00@gmail.com>
2018-02-19 00:49:32 -0500

    succesfully converted to more organized dir structure. Began setting up routes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+//require('./routes/apiRoutes')(app)
+require('./routes/htmlRoutes')(app)
+
 app.listen(PORT, function(){
     console.log(`App listening on PORT: ${PORT}`)
 });
