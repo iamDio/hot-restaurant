c8143cf30da6f249dec19ed8c33b42c269d59c79
iamDio <claudio.villani00@gmail.com>
2018-02-18 23:49:42 -0500

    added server.js file. @TODO: restructure folder structure to resemble an MVC pattern

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(PORT, function(){
+    console.log(`App listening on PORT: ${PORT}`)
+});
