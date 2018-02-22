ebcd6ad4eb0671e4caa3ec63e698a89f4919f9b2
iamDio <claudio.villani00@gmail.com>
2018-02-22 13:06:50 -0500

    finished setting up html routes. Will write an example using a switch statement to showcase one way to make the code more DRY

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,15 @@ const path = require('path')
 
 
 module.exports=  function(app){
-    app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/reservation.html'))
+    app.get('/', function(req,res){
+        res.sendFile(path.join(__dirname,'../public/index.html'))
+    })
+    app.get('/reservation',function(req,res){
+        res.sendFile(path.join(__dirname,'../public/reservation.html'))
+    })
+    app.get('/waitlist', function(req,res){
+        res.sendFile(path.join(__dirname,'../public/waitList.html'))
     })
 }
+
+
