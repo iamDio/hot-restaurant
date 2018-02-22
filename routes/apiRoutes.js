029abc9ddab02ee6d202455f8f1295b023e43286
iamDio <claudio.villani00@gmail.com>
2018-02-22 13:46:47 -0500

    get routes fully functional with 2 different methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,26 @@ console.log(tables)
 const waitList = require('../data/waitListData.js')
 console.log(waitList)
 
+// module.exports = function(app){
+//     app.get('/api/tables', function(req,res){
+//         res.json(tables)
+//     })
+//     app.get('/api/waitlist', function(req,res){
+//         res.json(waitList)
+//     })
+// } 
+
 module.exports = function(app){
-    app.get('/api/tables', function(req,res){
-        res.json(tables)
+    app.get('/api/:endpoint?',function(req,res){
+        switch(req.params.endpoint){
+            case 'tables':
+                res.json(tables)
+                break
+            case 'waitlist':
+                res.json(waitList)
+                break
+            default:
+                res.send('404 not found')
+        }
     })
-    app.get('/api/waitlist', function(req,res){
-        res.json(waitList)
-    })
-} 
+}
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,35 @@
 const path = require('path')
 
 
-module.exports=  function(app){
-    app.get('/', function(req,res){
-        res.sendFile(path.join(__dirname,'../public/index.html'))
-    })
-    app.get('/reservation',function(req,res){
+// module.exports=  function(app){
+//     app.get('/', function(req,res){
+//         res.sendFile(path.join(__dirname,'../public/index.html'))
+//     })
+//     app.get('/reservation',function(req,res){
+//         res.sendFile(path.join(__dirname,'../public/reservation.html'))
+//     })
+//     app.get('/waitlist', function(req,res){
+//         res.sendFile(path.join(__dirname,'../public/waitList.html'))
+//     })
+// }
+
+
+module.exports = function(app){
+    app.get('/:endpoint?',function(req,res){
+        switch(req.params.endpoint){
+            case undefined:
+            case '/':
+                res.sendFile(path.join(__dirname,'../public/index.html'))
+                break
+            case 'reservation':
         res.sendFile(path.join(__dirname,'../public/reservation.html'))
-    })
-    app.get('/waitlist', function(req,res){
-        res.sendFile(path.join(__dirname,'../public/waitList.html'))
+                break
+            case 'waitList':
+                res.sendFile(path.join(__dirname,'../public/waitlist.html'))
+                break
+            default:
+                res.status(404).send('404 not found');
+            break
+        }
     })
 }
-
-
